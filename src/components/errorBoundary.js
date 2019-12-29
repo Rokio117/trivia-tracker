4a37f3d3cd6633cd472c7c27f104cfd343d46313
Nick Hillary <hillan02@gmail.com>
2019-12-29 16:36:52 -0600

    added context to error boundary

--- src/components/errorBoundary.js
+++ src/components/errorBoundary.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
-
+import TriviaContext from "../context";
 class Error extends Component {
   constructor(props) {
     super(props);
@@ -13,19 +13,27 @@ class Error extends Component {
   render() {
     if (this.state.hasError) {
       return (
-        <div>
-          <h2>Oops! Somethings went wrong</h2>
-          <button
-            onClick={() => {
-              const sessionInfo = JSON.parse(sessionStorage.getItem("state"));
-              if (sessionInfo) {
-                this.login(sessionInfo.user);
-              } else this.props.history.push("/");
-            }}
-          >
-            Got It
-          </button>
-        </div>
+        <TriviaContext.Consumer>
+          {value => {
+            return (
+              <div>
+                <h2>Oops! Somethings went wrong</h2>
+                <button
+                  onClick={() => {
+                    const sessionInfo = JSON.parse(
+                      sessionStorage.getItem("state")
+                    );
+                    if (sessionInfo) {
+                      this.login(sessionInfo.user);
+                    } else this.props.history.push("/");
+                  }}
+                >
+                  Got It
+                </button>
+              </div>
+            );
+          }}
+        </TriviaContext.Consumer>
       );
     }
     return this.props.children;
