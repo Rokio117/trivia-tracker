dc5222e183a9dc61a84d7dfb7bd6dad9c1b82001
Nick Hillary <hillan02@gmail.com>
2020-01-02 18:36:46 -0600

    changed app title

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-  API_ENDPOINT: "http://localhost:8000/api",
+  //API_ENDPOINT: "http://localhost:8000/api",
+  API_ENDPOINT: `https://agile-chamber-28782.herokuapp.com/api`,
   TOKEN_KEY: "trivia-tracker-auth-token"
 };
